8736b5173deba55ab30bb030144ef26cf6e02a69
miguel-merlin <mmerlin@stevens.edu>
2023-11-16 16:14:34 -0500

    Implemented count.c

--- src/count.c
+++ src/count.c
@@ -1,13 +1,60 @@
 #include "include/count.h"
 
 int count_lines(char *s) {
-    // Implement line counting
+   
+    /*
+    Count the number of liunes in a string. A line is defined as a sequence of
+    characters followed by a newline character ('\n').
+    */
+
+    int lines = 0;
+    while (*s != '\0')
+    {
+        if (*s == '\n')
+        {
+            lines++;
+        }
+        s++;
+    }
+    return lines;
 }
 
 int count_words(char *s) {
-    // Implement word counting
+
+    /*
+    Count the number of words in a string. A word is defined as a sequence of
+    non-whitespace characters.
+    */
+
+    int words = 0;
+    int in_word = 0;
+    while (*s)
+    {
+        if (isspace(*s))
+        {
+            in_word = 0;
+        }
+        else if (!in_word)
+        {
+            in_word = 1;
+            words++;
+        }
+        s++;
+    }
+    return words;
 }
 
 int count_characters(char *s) {
-    // Implement character counting
+
+    /*
+    Count the number of characters in a string. 
+    */
+
+    int characters = 0;
+    while (*s)
+    {
+        characters++;
+        s++;
+    }
+    return characters;
 }
